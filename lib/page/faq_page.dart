5dfcf8efa2533fd514894a832838d1ab9f1315ae
monica.shannon <monica.shannon.ms@gmail.com>
2022-12-07 16:47:01 +0700

    faq page tes

---
+++ lib/page/faq_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:flutter/material.dart';
+import 'package:getwidget/getwidget.dart';
+
+import 'package:sehat_terus/appbar.dart';
+import 'package:sehat_terus/core/colors.dart';
+import 'package:sehat_terus/models/article_model.dart';
+import 'package:sehat_terus/page/article_page.dart';
+import 'package:sehat_terus/page/data_statistik.dart';
+import 'package:sehat_terus/page/onboarding.dart';
+import 'package:sehat_terus/widget/section5m.dart';
+import 'package:sehat_terus/widget/image_container.dart';
+
+class FaqPage extends StatelessWidget {
+  const FaqPage({Key? key}) : super(key: key);
+  static const routeName = '/faq';
+
+  @override
+  Widget build(BuildContext context) {
+    
+   return Text("helloooooow");
+  }
+}
