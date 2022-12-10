201b2f80dac8f2631d4ee24d364f5683ed85d205
refianys1678 <refianys1678@gmail.com>
2022-12-10 22:58:30 +0700

    register page ui

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ import 'package:sehat_terus/page/nakes_page.dart';
 import 'package:sehat_terus/page/onboarding.dart';
 import 'package:sehat_terus/page/article_page.dart';
 import 'package:sehat_terus/page/faq_page.dart';
+import 'package:sehat_terus/page/signup_page.dart';
 
 void main() {
   runApp(const App());
@@ -49,6 +50,7 @@ class App extends StatelessWidget {
           '/blog': (context) => const BlogPage(),
           '/detailblog': (context) => const DetailBlogPage(),
           '/nakes': (context) => const NakesPage(),
+          '/signup': (context) => const SignUpApp(),
         },
       ),
     );
